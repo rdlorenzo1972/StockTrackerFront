99272e1ea1a0cc7dc701201cf41a604dec97a177
randy <randy@RandyLaptop>
2022-08-12 02:01:04 -0400

    Updated stockCtrlSignin. It was able to verify signin from backend. After signin, returns to same page and displays status code above form.

--- WEB_CourseProject/src/main/java/strockContoller/stockControllerSignin.java
+++ WEB_CourseProject/src/main/java/strockContoller/stockControllerSignin.java
@@ -1,11 +1,17 @@
 package strockContoller;
 
+import java.awt.print.Printable;
 import java.io.IOException;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.mail.MessagingException;
+import javax.servlet.RequestDispatcher;
+import javax.servlet.http.HttpSession;
+import java.net.URL;
+import java.net.HttpURLConnection;
 
 /**
  * Servlet implementation class stockControllerSignin
@@ -27,7 +33,36 @@ public class stockControllerSignin extends HttpServlet {
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+		
+		if (request.getParameter("action").equals("userSignin")) {
+			
+			String name = request.getParameter("user_name");
+			String password = request.getParameter("user_password");
+			String url = "https://dev13.jhuep.com/StockTracker/user/signin?email=" + name + "&password=" + password;
+			System.out.print(url);
+			URL obj = new URL(url);
+			HttpURLConnection con = (HttpURLConnection) obj.openConnection();
+			con.setRequestMethod("GET");
+			//con.setRequestProperty("User-Agent", USER_AGENT);
+			int responseCode = con.getResponseCode();
+			System.out.println("GET Response Code :: " + responseCode);
+			if (responseCode == 201) { // success
+				String message = "Was able to signin";
+				request.setAttribute("message",  message);
+				String goToUrl = "/index.jsp";
+				RequestDispatcher dispatcher = getServletContext().getRequestDispatcher(goToUrl);
+				dispatcher.forward(request, response);	
+			} else {
+				//String message = "NOT able to signin";
+				int message = response.getStatus();
+				request.setAttribute("message",  Integer.toString(message));
+				String goToUrl = "/index.jsp";
+				RequestDispatcher dispatcher = getServletContext().getRequestDispatcher(goToUrl);
+				dispatcher.forward(request, response);	
+			}
+		
+		}
+		
 	}
 
 	/**
